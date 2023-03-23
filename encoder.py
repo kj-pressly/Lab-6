8cda637fdcb4e1e44a5fac20c566386a8029582e
kj-pressly <127222875+kj-pressly@users.noreply.github.com>
2023-03-22 20:29:21 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,15 +1,12 @@
 # KJ Pressly 76143268
 
-
-def encoder():
-    password_input = (input("Please enter your password to encode:"))
+def encoder(password_input):
     encoded_password = ''
     for i in password_input:
         if int(i) >= 7:
             encoded_password += str(int(i) - 7)
         else:
             encoded_password += str(int(i) + 3)
-    print("Your password has been encoded and stored!")
     return encoded_password
 
 
