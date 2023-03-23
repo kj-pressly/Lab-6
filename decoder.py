bc88ea3454a2e8d040f6a09af9209b87cf491c95
justinNguyen <nguyenjustin@ufl.edu>
2023-03-22 20:24:29 -0400

    Added decoder function

---
+++ decoder.py
@@ -0,0 +1,10 @@
+
+def decoder(password):
+    decoded = ''
+    for i in range(0, len(password)):
+        if int(password[i]) > 2:
+            decoded += str((int(password[i]) + 7))
+        else:
+            decoded += str((int(password[i]) - 3))
+        print("The encoded password is " + password + ", and the original password is " + decoded + ".\n")
+    return decoded
